7d0c09e336d7ceb0ef810b8648ddfe43d0cee69f
67au <oyy6745@gmail.com>
2023-02-21 22:17:19 +0800

    minor change

--- tower-timer.js
+++ tower-timer.js
@@ -5,9 +5,9 @@ function getTowerFloors(time) {
     const week = time.getUTCDay();
     const hour = time.getUTCHours();
     const floors = [];
-    const n = Math.floor(hour / 5)+1;
+    const n = Math.floor(hour / 5) + 1;
     for (let i = 0; i < 5; i++) {
-        const growth = week<i?7+week-i:week-i;
+        const growth = (7 + week - i) % 7;
         floors.push({
             'kind': kinds[i],
             'floor': 15 + growth * 5 + (hour < 1 ? 0 : n)
