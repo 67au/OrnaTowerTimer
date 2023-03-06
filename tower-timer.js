a8216edf43cdf0aa75ace47374ec42cb75a4b41d
67au <oyy6745@gmail.com>
2023-03-07 03:31:36 +0800

    refactor: Refactor with bootstrap

--- tower-timer.js
+++ tower-timer.js
@@ -1,17 +1,25 @@
 const kinds = ["selene", "eos", "oceanus", "themis", "prometheus"];
 // 15, 45, 40, 35, 20 Mon
+const base_floor = 15;
 
+/**
+ * 计算塔层数
+ * @param { Date } time
+ * @param { Array<T> } floors
+ */
 function getTowerFloors(time) {
-    const week = time.getUTCDay();
+    const day = time.getUTCDay();
     const hour = time.getUTCHours();
-    const floors = [];
     const n = Math.floor(hour / 5) + 1;
-    for (let i = 0; i < 5; i++) {
-        const growth = (7 + week - i) % 7;
-        floors.push({
-            'kind': kinds[i],
-            'floor': 15 + growth * 5 + (hour < 1 ? 0 : n)
-        });
-    }
+    const base_reset_time = new Date(time);
+    base_reset_time.setUTCHours(0, 0, 0, 0);
+    const floors = kinds.map((element, index) => {
+        const growth = (7 + day - index) % 7;
+        return {
+            'kind': element,
+            'floor': base_floor + growth * 5 + (hour < 1 ? 0 : n),
+            'nextReset': new Date(base_reset_time.setUTCDate(time.getUTCDate() - day + (day < index ? 0 : 7) + index)),
+        }
+    })
     return floors;
 }
